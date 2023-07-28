6b1cd8014473a9f6bd64519529af92d6ec48708e
kamsinghGA <kamaljitga@gmail.com>
2023-07-27 20:37:16 -0400

    created NavBar and displayed it in the main app component

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import { useState } from 'react';
+import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
+  const [user, setUser] = useState(null);
   return (
     <main className="App">
-      <h1>Hello</h1>
+      <NavBar />
     </main>
   );
 }
