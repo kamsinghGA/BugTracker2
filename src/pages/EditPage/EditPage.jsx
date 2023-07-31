0c569016c563106c19fad067981124caf93ca87e
kamsinghGA <kamaljitga@gmail.com>
2023-07-31 17:52:36 -0400

    creating handleSaveChanges

--- src/pages/EditPage/EditPage.jsx
+++ src/pages/EditPage/EditPage.jsx
@@ -1,18 +1,49 @@
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export default function EditPage() {
     const {id} = useParams();
+    const [bug, setBug] = useState(null);
 
     const fetchBugs = async () => {
         const response = await fetch(`/api/bugs/${id}`)
         const json = await response.json()
 
         if(response.ok) {
-          console.log(json)
+          setBug(json)
         }
+    }
+    useEffect(() => {
+        fetchBugs();
+    }, [id]);
+
+
+
+    async function handleSaveChanges() {
+        try {
+            const response = await fetch(`/api/bugs/${id}`, {
+              method: 'PUT',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ name, description }),
+            });
+      
+            if (response.ok) {
+              console.log('Bug updated successfully!');
+            } else {
+              console.error('Failed to update bug.');
+            }
+          } catch (error) {
+            console.error('Error occurred while updating bug:', error);
+          }
     }
 
     return(
-        <h1>Bye</h1>
+        <>
+            <h1>Edit Bug Page</h1>
+        
+        </>
     )
 }
